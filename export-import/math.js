53fd64284fdc59465602502f7e9304ce4165a585
Parvez Hossien Tarafder <parveztarafder31@gmail.com>
2026-03-13 21:03:24 +0530

    export and import, module in js

---
+++ export-import/math.js
@@ -0,0 +1,13 @@
+export const add = (a, b) => {
+  return a + b;
+};
+
+export const multiply = (a, b) => {
+  return a * b;
+};
+
+const greet = () => {
+  return "hello world";
+};
+
+export default greet;
---
+++ export-import/result.js
@@ -0,0 +1,6 @@
+import { add as sum, multiply as multi } from "./math.js";
+import greet from "./math.js";
+
+console.log(sum(5, 6));
+console.log(multi(5, 6));
+console.log(greet());
